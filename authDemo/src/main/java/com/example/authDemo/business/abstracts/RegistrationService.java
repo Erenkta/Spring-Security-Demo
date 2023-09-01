b0b777df31b119e46141e3fef0e76f6a3bff9279
Eren Nokta <eren.nkta@gmail.com>
2023-09-01 14:26:17 +0300

    Registration Created and Commited
    Bu kısımda Registration için bir obje-service ve controller oluşturduk.
    RegistrationRequest olarak oluşturduğumuz Class'ı aslında bir userVM
    olarak düşünebiliriz çünkü User Class'ından sadece ihtiyacımız olanları
    alacağımız bir DTO objesi oluşturmuşuz gibi oldu. Tek farkı bunu ayrı
    bir class ile yaptık.

---
+++ authDemo/src/main/java/com/example/authDemo/business/abstracts/RegistrationService.java
@@ -0,0 +1,8 @@
+package com.example.authDemo.business.abstracts;
+
+import com.example.authDemo.entities.concretes.RegistrationRequest;
+
+public interface RegistrationService {
+	public String register(RegistrationRequest request);
+
+}
